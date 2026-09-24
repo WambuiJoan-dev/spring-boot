9e38d923075e4f86311e2862563f744478d2bb18
+ <wambuijoan44@gmail.com>
2026-09-24 15:00:48 +0300

    addscontructor

---
+++ src/main/java/com/joandev/SoftwareEngineer.java
@@ -0,0 +1,18 @@
+package com.joandev;
+
+public class SoftwareEngineer {
+    private Integer id;
+    private String name;
+    private String techStack;
+
+    public SoftwareEngineer() {
+    }
+
+    public SoftwareEngineer(Integer id,
+                            String name,
+                            String techStack) {
+        this.id = id;
+        this.name = name;
+        this.techStack = techStack;
+    }
+}
