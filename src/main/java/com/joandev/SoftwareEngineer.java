54518ff841e86ac6e6921a6d820870882265ee63
+ <wambuijoan44@gmail.com>
2026-09-24 15:07:28 +0300

    addsequalsandhashcode

--- src/main/java/com/joandev/SoftwareEngineer.java
+++ src/main/java/com/joandev/SoftwareEngineer.java
@@ -1,5 +1,7 @@
 package com.joandev;
 
+import java.util.Objects;
+
 public class SoftwareEngineer {
     private Integer id;
     private String name;
@@ -38,5 +40,17 @@ public class SoftwareEngineer {
 
     public void setTechStack(String techStack) {
         this.techStack = techStack;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SoftwareEngineer that = (SoftwareEngineer) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(techStack, that.techStack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, techStack);
     }
 }
